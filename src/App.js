f141fa0d75f1da02fb1f642febc2adb109444505
Leonhardt Koepsell <leonhardt@koepsell.io>
2018-09-03 19:05:39 -0400

    Add the logo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import {Router} from 'react-static'
 import {hot} from 'react-hot-loader'
 import Routes from 'react-static-routes'
+import beer from './beer.svg'
 
 import './app.css'
 
 const App = () => (
     <Router>
         <div className="app">
-            <h1 className="logo">logo</h1>
+            <a href="https://distribrewtion.com" className="logo">
+                <img src={beer} className="logo-icon"/>
+            </a>
             <div className="content">
                 <Routes/>
             </div>
