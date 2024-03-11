6b14d9fe04ad454f50875ab8ba373ad17cf97879
Leonhardt Koepsell <leonhardt@distribrewtion.com>
2024-03-11 11:56:22 -0400

    Disallow crawling
    Authored-by: Leonhardt Koepsell <leonhardt@distribrewtion.com>

--- src/app.tsx
+++ src/app.tsx
@@ -3,24 +3,29 @@ import './app.css'
 import 'typeface-roboto'
 
 import React from 'react'
-import {Root, Routes} from 'react-static'
+import {Head, Root, Routes} from 'react-static'
 import {Router} from '@reach/router'
 import beer from './beer.svg'
 
 export const App = () => (
-    <Root>
-        <div className="app">
-            <a href="https://distribrewtion.com" className="logo">
-                <img src={beer} alt="Distribrewtion"/>
-            </a>
+    <>
+        <Head>
+            <meta name="robots" content="noindex, nofollow"></meta>
+        </Head>
+        <Root>
+            <div className="app">
+                <a href="https://distribrewtion.com" className="logo">
+                    <img src={beer} alt="Distribrewtion"/>
+                </a>
 
-            <div className="content">
-                <React.Suspense fallback={<h1>Loading...</h1>}>
-                    <Router>
-                        <Routes path="*"/>
-                    </Router>
-                </React.Suspense>
+                <div className="content">
+                    <React.Suspense fallback={<h1>Loading...</h1>}>
+                        <Router>
+                            <Routes path="*"/>
+                        </Router>
+                    </React.Suspense>
+                </div>
             </div>
-        </div>
-    </Root>
+        </Root>
+    </>
 )
